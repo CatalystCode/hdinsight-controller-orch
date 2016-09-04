91ffacf16277b5f65c8e92686c8ad65b7a0e96a5
morsh <mor.shemesh@gmail.com>
2016-09-04 13:33:00 +0300

    wip 5

--- HDInsightOrchestrator/index.js
+++ HDInsightOrchestrator/index.js
@@ -2,6 +2,12 @@ var azure = require('azure-storage');
 var async = require('async');
 var request = require('request');
 
+//var FunctionsManager = require('../lib/manage-functions');
+//var HDInsightManager = require('../lib/manage-hdinsight');
+
+var lastInactiveCheck = null;
+var MAX_INACTIVE_TIME = 15; // Minutes
+
 module.exports = function (context, myTimer) {
     var timeStamp = new Date().toISOString();
     
@@ -11,12 +17,12 @@ module.exports = function (context, myTimer) {
     }
     context.log('Node.js timer trigger function ran!', timeStamp);
 
-    var config = null;
-    try {
-      config = require('../lib/config');
-    } catch (e) {
-      return context.done(e);
-    }   
+    // var config = null;
+    // try {
+    //   config = require('../lib/config');
+    // } catch (e) {
+    //   return context.done(e);
+    // }   
     
     context.log('config was read ok');
 
